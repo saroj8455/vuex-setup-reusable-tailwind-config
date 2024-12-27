f9c8ef7380c385334cf41ec859763431124c5b32
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-27 23:52:35 +0530

    update welcome

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,7 +4,7 @@
 	<section class="section">
 		<div class="p-8">
 			<h1 class="text-2xl font-bold text-center text-blue-500">
-				Vuex App Entry
+				Vuex App Entry - 1
 			</h1>
 		</div>
 	</section>
