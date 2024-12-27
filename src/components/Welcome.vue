d013d5f437e88f4713df4997a2e4d3b5cf33cdcf
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-27 23:54:02 +0530

    change revert

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,7 +4,7 @@
 	<section class="section">
 		<div class="p-8">
 			<h1 class="text-2xl font-bold text-center text-blue-500">
-				Vuex App Entry - 1
+				Vuex App Entry
 			</h1>
 		</div>
 	</section>
