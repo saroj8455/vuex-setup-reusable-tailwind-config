4caa34b83d69e16dd4cb98ad9237f4cd7fbd4d91
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2024-12-29 13:27:38 +0530

    implement post details with primary card custom class

--- src/App.vue
+++ src/App.vue
@@ -54,15 +54,69 @@ const generateRandom = () => {
 				{{ err }}
 			</div>
 			<!-- Post Content -->
-			<div
-				v-else-if="post"
-				class="bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden md:w-full px-2 py-8"
-			>
+			<div v-else-if="post" class="primary-card px-4 py-8">
 				<!-- avoid can't read properties title and body with optional chaining -->
 				<!-- <h3 class="subtitle">{{ post?.title }}</h3>
 				<p>{{ post?.body }}</p> -->
 				<h3 class="subtitle">{{ post.title }}</h3>
 				<p>{{ post.body }}</p>
+				<!-- Post Metadata -->
+				<div class="flex items-center space-x-4 text-gray-600 text-sm my-3">
+					<div class="flex items-center space-x-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							class="h-5 w-5 text-green-500"
+							viewBox="0 0 24 24"
+						>
+							<path
+								d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+							/>
+						</svg>
+						<span>{{ post.reactions.likes }} Likes</span>
+					</div>
+					<div class="flex items-center space-x-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							class="h-5 w-5 text-red-500"
+							viewBox="0 0 24 24"
+						>
+							<path
+								d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+							/>
+						</svg>
+						<span>{{ post.reactions.dislikes }} Dislikes</span>
+					</div>
+					<div class="flex items-center space-x-2">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							class="h-5 w-5 text-blue-500"
+							viewBox="0 0 24 24"
+						>
+							<path
+								d="M12 3C7.03 3 3 7.03 3 12s4.03 9 9 9 9-4.03 9-9-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7zm-.5-10h-1v6h5v-1h-4V9z"
+							/>
+						</svg>
+						<span>{{ post.views }} Views</span>
+					</div>
+				</div>
+				<!-- Metadata  -->
+				<!-- Post Tags -->
+				<div class="mt-4">
+					<!-- <h4 class="text-lg font-semibold text-gray-800">Tags:</h4> -->
+					<div class="flex flex-wrap gap-2 mt-2">
+						<span
+							v-for="tag in post.tags"
+							:key="tag"
+							class="px-3 py-1 bg-gray-100 text-gray-800 rounded-full text-sm"
+						>
+							{{ tag }}
+						</span>
+					</div>
+				</div>
+				<!-- Tags -->
 			</div>
 			<div v-else class="text-red-500 text-lg font-medium">
 				<p>No data available.</p>
